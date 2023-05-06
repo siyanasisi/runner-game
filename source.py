bec579e2355d194acdd7c8920b5be03fe020bbb1
siyanasisi <119444284+siyanasisi@users.noreply.github.com>
2023-05-06 22:25:06 +0300

    Sky surface

---
+++ source.py
@@ -0,0 +1,21 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("runner by @sisiii")
+clock = pygame.time.Clock()
+
+sky_surface = pygame.image.load('graphics/Sky.png')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+   
+    screen.blit(sky_surface,(0,0))
+
+    pygame.display.update()
+    clock.tick(60)
